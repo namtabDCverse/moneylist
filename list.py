d06d9fe46c5da802b9d447b31f2894014703be5a
Jimmy Hsu <iambatman0828@gmail.com>
2020-02-09 00:34:18 +0800

    add list

--- list.py
+++ list.py
@@ -4,11 +4,15 @@ while True:
 	if name == 'quit':
 		break
 	price = input('金額:')
+	price = int(price)
 	lists.append([name, price])
 	
 for l in lists:
 	print(l[0],l[1] ,'元')
 
-
+with open('lists.csv','w') as f:
+	f.write('商品,價格\n')
+	for l in lists:
+		f.write(l[0] + ',' + str(l[1]) + '\n')
 
   
