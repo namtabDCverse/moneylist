5284b5f16b405d85b337b27e92b340112ab3985d
Jimmy Hsu <iambatman0828@gmail.com>
2020-02-15 23:27:01 +0800

    main_function

--- list.py
+++ list.py
@@ -1,38 +1,52 @@
-lists = [] 
 
-#檢查檔案
 import os
 
-if os.path.isfile('lists.csv'):
-	print('檔案存在')
-	#讀取檔案
-	with open('lists.csv','r') as f:
+#讀取檔案
+def read_file(filename):
+	accounts = [] 
+	with open(filename,'r') as f:
 		for line in f:  #line 為 excel 內的每一行
 			if '商品,價格'in line:
 				continue
 			name, price = line.strip().split(',')
-			lists.append([name, price])
-	print(lists)
+			accounts.append([name, price])
+	print(accounts)
+	return accounts
 
-else:
-	print('檔案不存在')
+#增加帳目
+def input_file(accounts): 
+	while True:
+		name = input('購買的物品:')
+		if name == 'X':
+			break
+		price = input('金額:')
+		price = int(price)
+		accounts.append([name, price])
+	return accounts
 
-#記帳
-while True:
-	name = input('購買的物品:')
-	if name == 'quit':
-		break
-	price = input('金額:')
-	price = int(price)
-	lists.append([name, price])
-	
-for l in lists:
-	print(l[0],l[1] ,'元')
+#印出帳目
+def print_products(accounts):	
+	for l in accounts:
+		print(l[0],l[1] ,'元')
 
 #寫入檔案
-with open('lists.csv','w') as f:
-	f.write('商品,價格\n')
-	for l in lists:
-		f.write(l[0] + ',' + str(l[1]) + '\n')
+def write_file(filename, accounts):	
+	with open(filename,'w') as f:
+		f.write('商品,價格\n')
+		for l in accounts:
+			f.write(l[0] + ',' + str(l[1]) + '\n')
 
-  
+#main function
+def main(): 
+	filename = 'lists.csv'
+	if os.path.isfile(filename):
+		print('找到檔案了')
+		accounts = read_file(filename)
+	else:
+		print('找不到檔案')
+
+	accounts = input_file(accounts)
+	print_products(accounts)
+	write_file(filename, accounts)
+
+main()
